83d8af77680801da25c0545e1cf44727a80ee10e
engineboy <pallymaster14@gmail.com>
2014-01-24 21:33:40 -0700

    Added All Transformations

--- README.md
+++ README.md
@@ -7,15 +7,18 @@ Authors:
 
 Changelog:
 	0.0.1a:
-		- Implemented OpenGL
-		- Added Window
-		- Added Main Game Loop
-		- Added Vectors
-		- Added Matrices
-		- Added Quaternion
-		- Added new input method using LWJGL Input
-		- Started Rendering mechanics
-		- Implemented Shaders
-		- Implemented GLSL
-		- Implemented Uniforms
-		- Added Translation
+		+ Implemented OpenGL
+		+ Added Window
+		+ Added Main Game Loop
+		+ Added Vectors
+		+ Added Matrices
+		+ Added Quaternion
+		+ Added new input method using LWJGL Input
+		+ Started Rendering mechanics
+		+ Implemented Shaders
+		+ Implemented GLSL
+		+ Implemented Uniforms
+		+ Added Translation
+		+ Added Rotation
+		+ Added Scaling
+		+ Added All Transformations
--- src/com/matr1x/projectiridium/handlers/Transform.java
+++ src/com/matr1x/projectiridium/handlers/Transform.java
@@ -4,29 +4,61 @@ import com.matr1x.projectiridium.util.Matrix4f;
 import com.matr1x.projectiridium.util.Vector3f;
 
 public class Transform {
-	
-	private Vector3f translation;
-	
-	public Transform() {
-		translation = new Vector3f(0, 0, 0);
-	}
-	
-	public Matrix4f getTransformation() {
-		Matrix4f translationMatrix = new Matrix4f().initTranslation(translation.getX(), translation.getY(), translation.getZ());
-		
-		return translationMatrix;
+    
+    private Vector3f translation;
+    private Vector3f rotation;
+    private Vector3f scale;
+    
+    public Transform() {
+        translation = new Vector3f(0, 0, 0);
+        rotation = new Vector3f(0, 0, 0);
+        scale = new Vector3f(1, 1, 1);
+    }
+    
+    public Matrix4f getTransformation() {
+            Matrix4f translationMatrix = new Matrix4f().initTranslation(translation.getX(), translation.getY(), translation.getZ());
+            Matrix4f rotationMatrix = new Matrix4f().initRotation(rotation.getX(), rotation.getY(), rotation.getZ());
+            Matrix4f scaleMatrix= new Matrix4f().initScale(scale.getX(), scale.getY(), scale.getZ());
+            
+            return translationMatrix.mul(rotationMatrix.mul(scaleMatrix));
+    }
+    
+    
+    public Vector3f getTranslation() {
+    	return translation;
+    }
+    
+    public void setTranslation(Vector3f translation) {
+    	this.translation = translation;
+    }
+    
+    public void setTranslation(float x, float y, float z) {
+    	this.translation = new Vector3f(x, y, z);
+    }
+
+    public Vector3f getRotation() {
+    	return rotation;
+    }
+
+    public void setRotation(Vector3f rotation) {
+    	this.rotation = rotation;
+    }
+    
+    public void setRotation(float x, float y, float z) {
+    	this.rotation = new Vector3f(x, y, z);
+    }
+
+	public Vector3f getScale() {
+		return scale;
 	}
 
-	public Vector3f getTranslation() {
-		return translation;
-	}
-
-	public void setTranslation(Vector3f translation) {
-		this.translation = translation;
+	public void setScale(Vector3f scale) {
+		this.scale = scale;
 	}
 	
-	public void setTranslation(float x, float y, float z) {
-		this.translation = new Vector3f(x, y, z);
+	public void setScale(float x, float y, float z) {
+		this.scale = new Vector3f(x, y, z);
 	}
+
 
 }
